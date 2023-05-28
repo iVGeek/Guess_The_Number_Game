3db8218e55ae4c765e088d6125a646e96f086367
iVGeek <ianvincent916@gmail.com>
2023-05-28 10:42:53 -0700

    game code update

--- game.py
+++ game.py
@@ -1,83 +1,106 @@
 import random
-import time
-from colorama import init, Fore, Style
-
-# Initialize colorama
-init(autoreset=True)
+from termcolor import colored
+from pyfiglet import figlet_format
 
 class GameLevel:
-    def __init__(self, name, min_value, max_value, max_attempts):
+    def __init__(self, name, min_value, max_value, max_attempts, color):
         self.name = name
         self.min_value = min_value
         self.max_value = max_value
         self.max_attempts = max_attempts
+        self.color = color
 
 def play_game(player1, player2=None):
     if player2:
-        print(Fore.CYAN + "Player 1: {}, Player 2: {}\n".format(player1, player2))
+        print("Player 1: {}, Player 2: {}\n".format(player1, player2))
     else:
-        print(Fore.CYAN + "Player: {}\n".format(player1))
+        print("Player: {}\n".format(player1))
 
     level = select_game_level()
     target_number = random.randint(level.min_value, level.max_value)
     guess_count = 0
+    attempts_left = level.max_attempts
 
     while True:
         if player2:
             current_player = player1 if guess_count % 2 == 0 else player2
-            print(Fore.YELLOW + "Current Player: {}".format(current_player))
+            print("Current Player: {}".format(current_player))
         else:
             current_player = player1
-        guess = int(input(Fore.WHITE + "Guess a number between {} and {}: ".format(level.min_value, level.max_value)))
+        guess = int(input(colored("Guess a number between {} and {}: ".format(level.min_value, level.max_value), "blue")))
         guess_count += 1
+        attempts_left -= 1
 
         if guess < target_number:
-            print(Fore.RED + "Too low!")
+            print(colored("Too low!", "red"))
         elif guess > target_number:
-            print(Fore.RED + "Too high!")
+            print(colored("Too high!", "red"))
         else:
-            print(Fore.GREEN + "Congratulations, {}! You guessed the number in {} tries!".format(current_player, guess_count))
-            confetti_animation()
-            play_again = input(Fore.YELLOW + "Do you want to play again? (y/n): ")
-            if play_again.lower() == 'y':
-                play_game(player1, player2)
-            else:
-                print(Fore.CYAN + "Thank you for playing!")
-                break
+            print(colored("Congratulations, {}! You guessed the number in {} tries!".format(current_player, guess_count), "green"))
+            # Add confetti using pyfiglet
+            confetti = colored(figlet_format("Congratulations!", font="starwars"), "yellow")
+            print(confetti)
+            break
 
-        if guess_count == level.max_attempts:
-            print(Fore.RED + "Game over! The correct number was {}.".format(target_number))
-            play_again = input(Fore.YELLOW + "Do you want to play again? (y/n): ")
-            if play_again.lower() == 'y':
-                play_game(player1, player2)
-            else:
-                print(Fore.CYAN + "Thank you for playing!")
-                break
+        if attempts_left == 0:
+            print(colored("Game over! The correct number was {}.".format(target_number), "red"))
+            break
+
+    play_again = input(colored("Do you want to play again? (y/n): ", "blue"))
+    if play_again.lower() == 'y':
+        if player2:
+            play_game(player1, player2)
+        else:
+            play_game(player1)
+    else:
+        print(colored("Thank you for playing!", "cyan"))
 
 def select_game_level():
-    print(Fore.CYAN + "Select a game level:")
-    print(Fore.YELLOW + "1. Easy (1-10, 6 attempts)")
-    print(Fore.YELLOW + "2. Medium (1-50, 4 attempts)")
-    print(Fore.YELLOW + "3. Hard (1-100, 3 attempts)")
-    choice = input(Fore.WHITE + "Enter the level number: ")
+    print("Select a game level:")
+    print(colored("1. Easy (1-10, 8 attempts)", "green"))
+    print(colored("2. Medium (1-50, 6 attempts)", "yellow"))
+    print(colored("3. Hard (1-100, 4 attempts)", "red"))
+    choice = input(colored("Enter the level number: ", "blue"))
 
     if choice == '1':
-        return GameLevel("Easy", 1, 10, 6)
+        return GameLevel("Easy", 1, 10, 8, "green")
     elif choice == '2':
-        return GameLevel("Medium", 1, 50, 4)
+        return GameLevel("Medium", 1, 50, 6, "yellow")
     elif choice == '3':
-        return GameLevel("Hard", 1, 100, 3)
+        return GameLevel("Hard", 1, 100, 4, "red")
     else:
-        print(Fore.RED + "Invalid choice. Defaulting to Medium level.")
-        return GameLevel("Medium", 1, 50, 4)
+        print(colored("Invalid choice. Defaulting to Medium level.", "red"))
+        return GameLevel("Medium", 1, 50, 4, "yellow")
 
-# Confetti animation
-def confetti_animation():
-    confetti_colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
+# Two-player mode
+def play_two_players():
+    print(colored("Two-Player Mode\n", "cyan"))
+    player1 = input(colored("Enter Player 1 name: ", "blue"))
+    player2 = input(colored("Enter Player 2 name: ", "blue"))
+    print(colored("\nLet's begin!\n", "cyan"))
+    play_game(player1, player2)
 
-    for _ in range(10):
-        confetti = ""
-        for _ in range(10):
-            confetti += random.choice(confetti_colors) + " * "
-       
+# Single-player mode against AI
+def play_with_ai():
+    print(colored("Single-Player Mode against AI\n", "cyan"))
+    player1 = input(colored("Enter your name: ", "blue"))
+    print(colored("\nLet's begin!\n", "cyan"))
+    play_game(player1)
+
+# Main menu
+def main():
+    print(colored("Welcome to Guess the Number!\n", "cyan"))
+    print(colored("1. Two-Player Mode", "magenta"))
+    print(colored("2. Single-Player Mode against AI", "magenta"))
+    choice = input(colored("Enter your choice (1 or 2): ", "blue"))
+
+    if choice == '1':
+        play_two_players()
+    elif choice == '2':
+        play_with_ai()
+    else:
+        print(colored("Invalid choice. Please try again.", "red"))
+
+if __name__ == '__main__':
+    main()
 
